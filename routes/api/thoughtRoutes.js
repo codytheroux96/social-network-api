7054264e7df27f5386a5f97cc7f9b56f5f23263b
cdt96 <cody.theroux3@gmail.com>
2022-07-17 13:47:25 -0400

    listed all of the routes for userRoutes and thoughtRoutes that will be brought into my controllers

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,13 @@
 const router = require('express').Router();
 
-
+const {
+  getThoughById,
+  getAllThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require('../../controllers/thoughtController');
 
 module.exports = router;
--- routes/api/userRoutes..js
+++ routes/api/userRoutes..js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
+const {
+  getUserbyId,
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/userController')
 
 
 module.exports = router;
