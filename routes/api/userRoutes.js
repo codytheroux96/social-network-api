1a7c1da5406cb5d3644d6b22fe0f07cc34f61f65
cdt96 <cody.theroux3@gmail.com>
2022-07-18 21:29:14 -0400

    added a colon in front of friendsId in userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,6 +12,6 @@ const {
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
-router.route('/:userId/friends/friendId').post(addFriend).delete(deleteFriend);
+router.route('/:id/friends/:friendsId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
