359053d678253b2316c6fa4e42bd238bcf006900
cdt96 <cody.theroux3@gmail.com>
2022-07-19 09:40:33 -0400

    fixed my route for deleteReaction

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,6 +12,6 @@ const {
 
 router.route('/').get(getAllThoughts).post(createThought);
 router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
-router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId?').post(addReaction).delete(deleteReaction);
 
 module.exports = router;
